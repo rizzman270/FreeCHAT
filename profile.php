34e3959e72d51aad2ae7cf90550381d56cbd1598
13MAD86, Martin <rikkuff2000@yahoo.de>
2025-09-07 16:37:53 +0200

    Update profile.php
    return right gender and language...

--- profile.php
+++ profile.php
@@ -19,6 +19,41 @@
 		$message=$lang["message"]["profile"];
 	}
 
+
+	if ($users[$user]["gender"] == "fa-venus-mars")
+		$gender_select1="selected";
+	else if ($users[$user]["gender"] == "fa-venus-double")
+		$gender_select2="selected";
+	else if ($users[$user]["gender"] == "fa-venus")
+		$gender_select3="selected";
+	else if ($users[$user]["gender"] == "fa-transgender")
+		$gender_select4="selected";
+	else if ($users[$user]["gender"] == "fa-non-binary")
+		$gender_select5="selected";
+	else if ($users[$user]["gender"] == "fa-neuter")
+		$gender_select6="selected";
+	else if ($users[$user]["gender"] == "fa-mercury")
+		$gender_select7="selected";
+	else if ($users[$user]["gender"] == "fa-mars-stroke-up")
+		$gender_select8="selected";
+	else if ($users[$user]["gender"] == "fa-mars-stroke-right")
+		$gender_select9="selected";
+	else if ($users[$user]["gender"] == "fa-mars-double")
+		$gender_select10="selected";
+	else if ($users[$user]["gender"] == "fa-mars-and-venus")
+		$gender_select11="selected";
+	else if ($users[$user]["gender"] == "fa-mars")
+		$gender_select12="selected";
+	else if ($users[$user]["gender"] == "fa-genderless")
+		$gender_select13="selected";
+
+	if ($users[$user]["language"] == "de_de")
+		$lang_select1="selected";
+	else if ($users[$user]["language"] == "en_us")
+		$lang_select2="selected";
+	else if ($users[$user]["language"] == "zh_cn")
+		$lang_select3="selected";
+
 	echo '
 		<form method="post" enctype="multipart/form-data">
 			<div class="w3-theme-white">
@@ -33,28 +68,28 @@
 				<div class="w3-half">
 					<div class="w3-row-padding w3-margin-bottom">
 						<select class="w3-select" name="gender">
-							<option value="fa-venus-mars">Hetero</option>
-							<option value="fa-venus-double">Lesbian</option>
-							<option value="fa-venus">Female</option>
-							<option value="fa-transgender">Transgender</option>
-							<option value="fa-non-binary">Genderqueer</option>
-							<option value="fa-neuter">Neutrois</option>
-							<option value="fa-mercury">Hermaphrodit</option>
-							<option value="fa-mars-stroke-up">Androgyne</option>
-							<option value="fa-mars-stroke-right">Othergender</option>
-							<option value="fa-mars-double">Gay</option>
-							<option value="fa-mars-and-venus">Bigender</option>
-							<option value="fa-mars">Male</option>
-							<option value="fa-genderless">Genderless</option>
+							<option value="fa-venus-mars" '. $gender_select1 .'>Hetero</option>
+							<option value="fa-venus-double" '. $gender_select2 .'>Lesbian</option>
+							<option value="fa-venus" '. $gender_select3 .'>Female</option>
+							<option value="fa-transgender" '. $gender_select4 .'>Transgender</option>
+							<option value="fa-non-binary" '. $gender_select5 .'>Genderqueer</option>
+							<option value="fa-neuter" '. $gender_select6 .'>Neutrois</option>
+							<option value="fa-mercury" '. $gender_select7 .'>Hermaphrodit</option>
+							<option value="fa-mars-stroke-up" '. $gender_select8 .'>Androgyne</option>
+							<option value="fa-mars-stroke-right" '. $gender_select9 .'>Othergender</option>
+							<option value="fa-mars-double" '. $gender_select10 .'>Gay</option>
+							<option value="fa-mars-and-venus" '. $gender_select11 .'>Bigender</option>
+							<option value="fa-mars" '. $gender_select12 .'>Male</option>
+							<option value="fa-genderless" '. $gender_select13 .'>Genderless</option>
 						</select>
 					</div>
 				</div>
 				<div class="w3-half">
 					<div class="w3-row-padding w3-margin-bottom">
 						<select class="w3-select" name="language">
-							<option value="de_de">'. $lang["lang"]["de_de"] .'</option>
-							<option value="en_us">'. $lang["lang"]["en_us"] .'</option>
-							<option value="zh_cn">'. $lang["lang"]["zh_cn"] .'</option>
+							<option value="de_de" '. $lang_select1 .'>'. $lang["lang"]["de_de"] .'</option>
+							<option value="en_us" '. $lang_select2 .'>'. $lang["lang"]["en_us"] .'</option>
+							<option value="zh_cn" '. $lang_select3 .'>'. $lang["lang"]["zh_cn"] .'</option>
 						</select>
 					</div>
 				</div>
